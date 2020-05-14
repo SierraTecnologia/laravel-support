2af4452e4452a5ab91025ebaf12eb30cbe8771ee
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-05-14 16:58:32 -0300

    Esboco Das novas funcionalidades

---
+++ src/_Patterns/Writer/Eloquent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Support\Components\Database\Render;
+
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\TableDiff;
+use Support\Components\Database\Schema\SchemaManager;
+use Support\Components\Database\Schema\Table;
+use Support\Components\Database\Types\Type;
+use Support\Traits\Debugger\DevDebug;
+use Support\Traits\Debugger\HasErrors;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use ReflectionClass;
+use ReflectionMethod;
+use Illuminate\Support\Collection;
+use SierraTecnologia\Crypto\Services\Crypto;
+use Illuminate\Http\Request;
+use App;
+use Log;
+use Artisan;
+use Support\Elements\Entities\DataTypes\Varchar;
+use Support\Elements\Entities\EloquentColumn;
+use Support\Components\Coders\Parser\ParseModelClass;
+use Symfony\Component\Inflector\Inflector;
+
+use Doctrine\DBAL\Schema\SchemaException;
+use Doctrine\DBAL\DBALException;
+use Symfony\Component\Debug\Exception\FatalThrowableError;
+use Symfony\Component\Debug\Exception\FatalErrorException;
+use Exception;
+use ErrorException;
+use LogicException;
+use OutOfBoundsException;
+use RuntimeException;
+use TypeError;
+use Throwable;
+use Watson\Validating\ValidationException;
+use Illuminate\Contracts\Container\BindingResolutionException;
+
+class Eloquent
+{
+
+}
