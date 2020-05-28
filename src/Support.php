63c3a3536d67447789d3c3556ea504760ece4521
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-05-28 17:59:25 -0300

    Salvando

--- src/Support.php
+++ src/Support.php
@@ -303,7 +303,7 @@ class Facilitador
 
             // Loop through all the packages and get the version of facilitador
             foreach ($file->packages as $package) {
-                if ($package->name == 'tcg/facilitador') {
+                if ($package->name == 'facilitador') {
                     $this->version = $package->version;
                     break;
                 }
